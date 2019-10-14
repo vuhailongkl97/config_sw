410dcb2c3630ca072927dfc0fe98fa08a0590e0f
dzs local <dzs@dzs.com>
2019-10-13 18:55:21 -0700

    update script auto startup tmux after use ssh

--- install.sh
+++ install.sh
@@ -1,15 +1,30 @@
 #!/bin/bash 
 
 VIM_DIR=$HOME/.vim
+BASH_RC_DIR=$HOME/.bashrc
+
 echo "installing vundle plugin"
-if [ -d "$HOME/.vim/bundle/Vundle.vim" ]; then 
-	echo "directory is exist"
+if [ -d "$VIM_DIR/bundle/Vundle.vim" ]; then 
+	echo "directory is exist, you only need clone config file"
+	./sync.sh
 else 
 	echo "create dir"
-	mkdir -p $HOME/.vim/bundle/
+	mkdir -p $VIM_DIR/bundle/
 	git clone https://github.com/VundleVim/Vundle.vim.git $VIM_DIR/bundle/Vundle.vim
 fi
+
+if [ -z "$(which xclip)" ]; then 
+	echo "installing xclip for control clipboard with tmux"
+	sudo apt-get install -y xclip
+fi
+
+if [ ! -f "$BASH_RC_DIR" ]; then 
+	echo "file $BASH_RC_DIR is not exist"
+else 
+
+	echo "update startup script for tmux in .bashrc"
+	cat .auto_startup_tmux.conf >> $BASH_RC_DIR
+fi
+
 # install all plugin in list .vundle.vim
 vim +PluginInstall +qall
-
-
